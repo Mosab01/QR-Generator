87ef3aebfdbb7f9f2608447b6e8d886aa8acb958
mosab01 <mosabkhraim3@gmail.com>
2023-09-10 22:31:29 +0300

    init

---
+++ README.md
@@ -0,0 +1,31 @@
+# QR Code Generator Site using React  
+This is a QR code generator website that is built using React and a REST API.  
+This website allows users to create QR codes by entering a URL, text or any other information they would like to encode.  
+Users can also change the color of the QR code and the background color, and download the generated QR code as a PNG file.
+
+# Installation
+Clone the repository to your local machine  
+Install the required packages using "npm install"  
+Start the server using "npm start"
+# Usage
+Open your browser and navigate to http://localhost:3000  
+Enter the text or URL you want to encode in the text box  
+Click on the "Generate QR Code" button  
+Use the color pickers to select the color for the QR code and background  
+Click on the "Download" button to download the generated QR code as a PNG file  
+
+# Technologies Used
+This QR code generator site is built using the following technologies:
+React  
+open Qr_generator API  
+JavaScript  
+HTML  
+CSS
+
+# Contributions
+Contributions are welcome! If you would like to contribute to this project, please open an issue or create a pull request.
+# on desktop  
+![qr_decktop](https://user-images.githubusercontent.com/90719835/226694127-8cf0269a-a3a6-461c-bced-9585e849c466.png)  
+
+# on phone  
+![qr_phone](https://user-images.githubusercontent.com/90719835/226694170-95546472-78af-415e-8df4-00c13b701e3e.png)  
---
+++ src/App.js
@@ -0,0 +1,63 @@
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+  const [data, setData] = useState("URL HERE");
+  const [color, setColor] = useState("");
+  const [bgcolor, setbgColor] = useState("");
+
+
+  const set_data = () =>{
+    return(`https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${data}&color=${color}&bgcolor=${bgcolor}&format=png`);
+  }
+
+
+ 
+  return (
+    <div className="App">
+      <h1 id='title'>QR Generator</h1>
+      <img src={set_data()} alt="Type somthing"/>
+      <div className='input-fields'>
+
+        <div className='input-field'>
+          <label>URL Name: </label>
+          <input 
+          id='data' 
+          value={data} 
+          placeholder="URL here"
+          onChange={(e) => setData(e.target.value)}
+          />
+        </div>
+
+        <div className='input-field'>
+          <label>Color: </label>
+          <input 
+          id='color'
+          value={color}
+          placeholder="example: 00ADB5"
+          onChange={(event) => setColor(event.target.value)}
+          />
+        </div>
+
+        <div className='input-field'>
+          <label>BgColor: </label>
+          <input 
+          id='bgcolor'
+          value={bgcolor}
+          placeholder="example: 222831"
+          onChange={(event) => setbgColor(event.target.value)}
+          />
+        </div>
+
+      </div>
+
+      <a id='download-btn' href={set_data()} download="download" target="_blank">
+        <button>download
+        </button>
+        </a>
+
+
+    </div>
+  );
+}
+export default App;
